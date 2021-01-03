f9748b91a3b85026a8b1b40891d5ced4865492dd
Luis Darli <luishenriquedarli@gmail.com>
2021-01-03 15:35:19 -0300

    add responsive

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,10 +2,14 @@ import React, { useState } from 'react';
 import { Checkbox } from '../Checkbox';
 import { Dropdown } from '../Dropdown';
 import { Tags } from '../Tags';
+import { useForm } from "react-hook-form"; // VALIDACAO DO FORM
 import './style.css'
 
 
 export const App = () => {
+    //metodos validacao do form
+    const { register, handleSubmit, errors } = useForm();
+
     //dados formulario
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
@@ -16,12 +20,12 @@ export const App = () => {
         id: "1",
         name: "Opção 1",
         selected: true
-    },{
-        id:"2",
+    }, {
+        id: "2",
         name: "Opção 2",
         selected: true
-    },{
-        id:"3",
+    }, {
+        id: "3",
         name: "Opção 3",
         selected: true
     }];
@@ -30,9 +34,10 @@ export const App = () => {
 
     //opcoes dropdown
     const [select, setSelect] = useState(0);
-    const dropdownOptions = ["Select 1", "Select 2", "Select 3"]
-    
-    
+    const dropdownOptions = ["Select 1", "Select 2", "Select 3"];
+
+
+
     //setando TAGS
     const [selectedOptions, setSelectedOptions] = useState([]);
 
@@ -42,32 +47,28 @@ export const App = () => {
     }, {
         id: "5fecf51da4c1fc6087ad0f6f",
         text: "iphone"
-    },{
-        id: "5febb0d786c6bc9cc54ba973",
-        text: "web",
-    },{
+    }, {
         id: "5fecf522d27b6416bb57877c",
         text: "interface"
-    },{
+    }, {
         id: "5fecf52631cc2372fbb294e3",
         text: "icon"
-    },{
+    }, {
         id: "5febb0d786c6bc9cc54ba977",
         text: "illustration"
-    },{
+    }, {
         id: "5febb0d786c6bc9cc54ba978",
         text: "graphics"
-    },{
+    }, {
         id: "5febb0d786c6bc9cc54ba97b",
         text: "ui"
-    },{
+    }, {
         id: "5febb0d786c6bc9cc54ba97d",
         text: "design"
-    },{
-        id:"5febb0d786c6bc9cc54ba97f",
+    }, {
+        id: "5febb0d786c6bc9cc54ba97f",
         text: "app"
     }];
-
 
     const handleTagOnClick = (option) => {
         let newArray = [];
@@ -83,14 +84,14 @@ export const App = () => {
         setSelectedOptions(newArray);
     };
 
-    const checkOnClick = (el) =>{
+    const checkOnClick = (el) => {
         let newArray = [];
 
         const isSelected = selectedOptionsCheckbox.find((item) => item.id === el.id); //opção ja marcada.
 
-        if(isSelected){
+        if (isSelected) {
             newArray = selectedOptionsCheckbox.filter((item) => item.id !== el.id); //retorna novo array sem o elemento ja selecionado.
-        }else{
+        } else {
             newArray = [...selectedOptionsCheckbox, el] //add elemento ao array caso selecionado
         }
         setSelectedOptionCheckbox(newArray);
@@ -107,40 +108,55 @@ export const App = () => {
         }
 
         fetch('https://api.trello.com/1/cards?key=e5a76519417cd6e9c33d5b986f3074a6&token=' + token,
-        {
-            method:"POST",
-            body: JSON.stringify(newCard),
-            headers: {
-                'Content-Type': 'application/json',
-            }
-        })
+            {
+                method: "POST",
+                body: JSON.stringify(newCard),
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            }).then((res) => {
+                console.log(res);
+                setName('');
+                setEmail('');
+                setMessage('');
+                alert('Cartão criado em : https://trello.com/b/1rs1WVJZ/api-test')
+            })
     };
 
     return (
         <div className="app">
             <section className="app__section">
-                <label>Name</label>
-                <input type="text" placeholder="John Doe" onChange={(e) => { setName(e.target.value) }} />
+                <form onSubmit={handleSubmit(handleSubmitClick)}>
+                    <section className="app__section">
+                        <label>Name</label>
+                        <div className="app__section-inputGroup">
+                        <input className={errors.name ? 'error' : 'sucess'} type="text" name="name" id="name" placeholder="John Doe" ref={register({ required: true })} onChange={(e) => { setName(e.target.value) }} value={name} />
+                        {errors.name ? <span className="app__section__inputGroup-error">X</span> : <span className="app__section__inputGroup-sucess">✓</span>}
+                        </div>
 
-                <label>E-mail</label>
-                <input type="text" placeholder="error@mail.com" onChange={(e) => { setEmail(e.target.value) }} />
+                        <label>E-mail</label>
+                        <div className="app__section-inputGroup">
+                        <input className={errors.email ? 'error' : 'sucess'} type="email" name="email" id="email" placeholder="error@mail.com" ref={register({ required: true })} onChange={(e) => { setEmail(e.target.value) }} value={email} />
+                        {errors.email ? <span className="app__section__inputGroup-error">X</span> : <span className="app__section__inputGroup-sucess">✓</span>}
+                        </div>
 
-                <textarea rows="10" cols="60" onChange={(e) => { setMessage(e.target.value) }} />
-            </section>
+                        <textarea rows="10" cols="60" onChange={(e) => { setMessage(e.target.value) }} value={message} />
+                    </section>
+                    <section className="app__section">
+                        <div className="app__section-checkbox">
+                            <Checkbox options={checkboxOptions} selected={selectedOptionsCheckbox} onClick={checkOnClick} />
+                        </div>
 
-            <section className="app__section">
-                <div className="app__section-checkbox">
-                    <Checkbox options={checkboxOptions} selected={selectedOptionsCheckbox} onClick={checkOnClick}/>
-                </div>
+                        <div className="app__section-dropdown">
+                            <Dropdown options={dropdownOptions} selected={select} onChange={(index) => { setSelect(index) }} />
+                        </div>
 
-                <div className="app__section-dropdown">
-                    <Dropdown options={dropdownOptions} selected={select} onChange={(index) => { setSelect(index) }} />
-                </div>
-
-                <div className="app__section-tags">
-                    <Tags options={tags} selectedOptions={selectedOptions} onClick={handleTagOnClick} />
-                    <button className="app__section-button" onClick={handleSubmitClick}>Enviar</button>
-                </div>
+                        <div className="app__section-tags">
+                            <Tags options={tags} selectedOptions={selectedOptions} onClick={handleTagOnClick} />
+                            <button type="submit" className="app__section-button">Enviar</button>
+                        </div>
+                    </section>
+                </form>
             </section>
         </div>
     )
--- src/components/Checkbox/index.js
+++ src/components/Checkbox/index.js
@@ -6,10 +6,9 @@ export const Checkbox = ({options, selected, onClick}) =>{
         <> 
             {
             options.map((option) =>{
-                
-                let isSelected = selected.find((el) => el.id === option.id); 
+                let isSelected = selected.find((el) => el.id === option.id);
 
-                return(
+                  return(
                     <div className="app__section__item" key={option.id} onClick={() => onClick(option)}>
                         <div className="app__section__item-selection">
                             {isSelected && <span>✓</span> }
